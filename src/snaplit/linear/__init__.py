ee1e186b57a2d0ebc6936ba50259b55b0f1dcef1
Simon Stensgaard <STcena1@hotmail.com>
2025-10-06 20:48:52 +0200

    Completed Python shim & docstring for Queue

--- src/snaplit/linear/__init__.py
+++ src/snaplit/linear/__init__.py
@@ -2,12 +2,14 @@
 
 from .linked_list import LinkedList
 from .py_stack import Stack
+from .py_queue import Queue
 
 #---------- Package Management ----------
 
 __all__ = [
     "LinkedList",
-    "Stack"
+    "Stack",
+    "Queue"
 ]
 __version__ = "0.1.1"
 __author__ = "HysingerDev"
