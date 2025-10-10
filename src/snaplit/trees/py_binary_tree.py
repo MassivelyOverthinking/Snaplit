e2e80f735d66d87689dd268382aea33d63a7f948
Simon Stensgaard <STcena1@hotmail.com>
2025-10-10 22:58:29 +0200

    Finalized BST shim methods - Starting on docstring

--- src/snaplit/trees/py_binary_tree.py
+++ src/snaplit/trees/py_binary_tree.py
@@ -2,7 +2,7 @@
 
 from rust_snaplit import BinarySearchTree as _RustBST
 
-from typing import Any, Optional, List, Iterable
+from typing import Any, List, Iterable
 
 #---------- Binary Search Tree Shim ----------
 
@@ -12,67 +12,73 @@ class BinarySearchTree():
         self._inner = _RustBST(allow_duplicates=allow_duplicates)
 
     def add(self, value: Any) -> None:
-        pass
+        self._inner.add(value)
 
     def remove(self, value: Any) -> Any:
-        pass
+        return self._inner.remove(value)
 
-    def prune(self) -> Any:
-        pass
+    def prune(self) -> None:
+        self._inner.prune()
 
     def peek_root(self) -> Any:
-        pass
+        return self._inner.peek_root()
 
     def contains(self, value: Any) -> bool:
-        pass
+        return self._inner.contains(value)
 
     def extend(self, elements: Iterable) -> None:
-        pass
+        self._inner.extend(elements)
 
     def min(self) -> Any:
-        pass
+        return self._inner.min()
 
     def max(self) -> Any:
-        pass
+        return self._inner.max()
 
     def at_depth(self, value: Any) -> int:
-        pass
+        return self._inner.at_depth(value)
 
     def height(self) -> int:
-        pass
+        return self._inner.height()
 
     def size(self) -> int:
-        pass
+        return self._inner.size()
 
     def is_empty(self) -> bool:
-        pass
+        return self._inner.is_empty()
 
     def inorder_list(self) -> List[Any]:
-        pass
+        return self._inner.inorder_list()
 
     def preorder_list(self) -> List[Any]:
-        pass
+        return self._inner.preorder_list()
 
     def postorder_list(self) -> List[Any]:
-        pass
+        return self._inner.postorder_list()
 
     def BFS_list(self) -> List[Any]:
-        pass
+        return self._inner.bfs_list()
 
     def copy(self) -> "BinarySearchTree":
-        pass
+        new_instance = self._inner.copy()
+        new_tree = self.__class__.__new__(self.__class__)
+        new_tree._inner = new_instance
+        return new_tree
 
     def clear(self) -> None:
-        pass
+        self._inner.clear()
 
     def __len__(self) -> int:
-        pass
+        return self._inner.size()
 
     def __bool__(self) -> bool:
-        pass
+        return not self._inner.is_empty()
 
     def __contains__(self, value: Any) -> bool:
-        pass
+        return self._inner.contains(value)
+    
+    def __iter__(self):
+        return iter(self.inorder_list())
 
     def __copy__(self) -> "BinarySearchTree":
-        pass
+        return self.copy()
