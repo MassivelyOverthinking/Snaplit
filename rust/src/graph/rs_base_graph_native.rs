5d5c48f82e13446af0c02cf1a7431aada7ac5e08
Simon Stensgaard <STcena1@hotmail.com>
2025-10-21 18:02:05 +0200

    Added edge methods and update functionality in BaseGraph

--- rust/src/graph/rs_base_graph_native.rs
+++ rust/src/graph/rs_base_graph_native.rs
@@ -29,6 +29,16 @@ pub struct BaseGraph {
     count: usize,
 }
 
+impl BaseGraph {
+    fn removal(&mut self, id: usize) {
+        for (_, item) in self.nodes.iter_mut() {
+            if item.neighbours.contains(&id) {
+                item.neighbours.remove(&id);
+            }
+        }
+    }
+}
+
 #[pymethods]
 impl BaseGraph {
     #[new]
@@ -61,6 +71,7 @@ impl BaseGraph {
         match self.nodes.remove(&key) {
             Some(value) => {
                 self.count -= 1;
+                Self::removal(self, key);
                 return Ok(value.payload.clone_ref(py))
             },
             None => return Err(PyValueError::new_err("Value not found in Graph")),
@@ -98,6 +109,78 @@ impl BaseGraph {
 
         let result = self.nodes.contains_key(&key);
         Ok(result)
+    }
+
+    pub fn update(&mut self, py: Python, item: PyObject, index: usize) -> PyResult<()> {
+        if self.nodes.is_empty() {
+            return Err(PyValueError::new_err("No elements currently available in Graph"));
+        }
+
+        let new_node = match self.nodes.get_mut(&index) {
+            Some(node_value) => node_value,
+            None => return Err(PyValueError::new_err("Index not found in present Graph"))
+        };
+
+        new_node.payload = item.clone_ref(py);
+        Ok(())
+    }
+
+    pub fn add_edge(&mut self, x: usize, y: usize) -> PyResult<()> {
+        if self.nodes.is_empty() {
+            return Err(PyValueError::new_err("No elements currently available in Graph"));
+        }
+
+        if !self.nodes.contains_key(&x) {
+            return Err(PyValueError::new_err("X node not found in current Graph"));
+        }
+
+        if !self.nodes.contains_key(&y) {
+            return Err(PyValueError::new_err("Y node not found in current Graph"));
+        }
+        
+        let x_node = self.nodes.get_mut(&x).unwrap();
+        x_node.neighbours.insert(y);
+        
+        let y_node = self.nodes.get_mut(&y).unwrap();
+        y_node.neighbours.insert(x);
+        
+        Ok(())
+    }
+
+    pub fn is_connected(&self, x: usize, y: usize) -> PyResult<bool> {
+        if self.nodes.is_empty() {
+            return Err(PyValueError::new_err("No elements currently available in Graph"));
+        }
+
+        if !self.nodes.contains_key(&x) {
+            return Err(PyValueError::new_err("X node not found in current Graph"));
+        }
+
+        if !self.nodes.contains_key(&y) {
+            return Err(PyValueError::new_err("Y node not found in current Graph"));
+        }
+
+        let x_node = self.nodes.get(&x).unwrap();
+        let y_node = self.nodes.get(&y).unwrap();
+        if x_node.neighbours.contains(&y) && y_node.neighbours.contains(&x) {
+            Ok(true)
+        } else {
+            Ok(false)
+        }
+    }
+
+    pub fn neighbours<'py>(&self, py: Python<'py>, index: usize) -> PyResult<&'py PyList> {
+        if self.nodes.is_empty() {
+            return Err(PyValueError::new_err("No elements currently available in Graph"));
+        }
+
+        let new_node = match self.nodes.get(&index) {
+            Some(value) => value,
+            None => return Err(PyValueError::new_err("Index not found in present Graph")),
+        };
+
+        let final_list = PyList::new(py, new_node.neighbours.clone());
+        Ok(final_list.into())
     }
 
     pub fn size(&self) -> PyResult<usize> {
