22fd04ce183643cd54cfaad752aa5d8086af316d
Simon Stensgaard <STcena1@hotmail.com>
2025-10-23 19:12:00 +0200

    Finished Python Shim and docstring for WeightedGraph-class

--- src/snaplit/graph/__init__.py
+++ src/snaplit/graph/__init__.py
@@ -2,12 +2,14 @@
 
 from .py_basegraph import BaseGraph
 from .py_digraph import Digraph
+from .py_weighted_graph import WeightedGraph
 
 #---------- Package Management ----------
 
 __all__ = [
     "BaseGraph",
     "Digraph",
+    "WeightedGraph",
 ]
 __version__ = "0.1.1"
 __author__ = "HysingerDev"
