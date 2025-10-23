55982e61d3b42247e631af54c8933ff69e6f1f54
Simon Stensgaard <STcena1@hotmail.com>
2025-10-23 18:29:59 +0200

    Started on Weighted Graph Shim in Python

---
+++ src/snaplit/graph/py_weighted_graph.py
@@ -0,0 +1,8 @@
+#---------- Imports ----------
+
+from rust_snaplit import WeightedGraph as _RustWeightedGraph
+
+from typing import Any, List, Tuple, Optional, Union
+
+#---------- Weighted Graph Shim ----------
+
