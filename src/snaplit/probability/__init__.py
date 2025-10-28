493116253da73f5401103dc7368c7aa0ffc3482d
Simon Stensgaard <STcena1@hotmail.com>
2025-10-28 17:42:07 +0100

    Finished Flatlist tests

--- src/snaplit/probability/__init__.py
+++ src/snaplit/probability/__init__.py
@@ -2,12 +2,14 @@
 
 from .py_bloom_filter import BloomFilter
 from .py_cuckoo_filter import CuckooFilter
+from .py_flatlist import Flatlist
 
 #---------- Package Management ----------
 
 __all__ = [
     "BloomFilter",
-    "CuckooFilter"
+    "CuckooFilter",
+    "Flatlist",
 ]
 __version__ = "0.1.1"
 __author__ = "HysingerDev"
