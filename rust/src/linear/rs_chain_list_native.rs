c02e2cd1e02f7d937145245ad5e8c3c4d61dcbd3
Simon Stensgaard <STcena1@hotmail.com>
2025-11-22 20:08:40 +0100

    Added free_list variables to ChainList-class in rust codebase

--- rust/src/linear/rs_chain_list_native.rs
+++ rust/src/linear/rs_chain_list_native.rs
@@ -4,9 +4,16 @@ use pyo3::PyObject;
 use pyo3::types::PyList;
 
 /// ---------------------------------------------------------------------------------
-/// Implementation of ChainLink helper class -> Operates like internal List Node
+/// Implementation of ChainLink helper class & Slot Enum
 /// ---------------------------------------------------------------------------------
 
+#[derive(Debug, Clone)]
+enum Slot {
+    Empty,
+    Occupied(ChainLink),
+}
+
+#[derive(Debug, Clone)]
 struct ChainLink {
     data: PyObject,
     next: usize,
@@ -23,22 +30,48 @@ pub struct ChainList {
     list_size: usize,
     head: usize,
     tail: usize,
-    list_array: Vec<PyObject>,
+    list_array: Vec<Slot>,
+    free_list: Vec<usize>,
 }
 
 #[pymethods]
 impl ChainList {
     #[new]
-    pub fn new(py: Python, capacity: Option<usize>) -> Self {
+    pub fn new(capacity: Option<usize>) -> Self {
         let cap = capacity.unwrap_or(1024);
-        let py_none = py.None();
         Self {
             capacity: cap,
             list_size: 0,
             head: 0,
             tail: 0,
-            list_array: vec![py_none; cap],
+            list_array: vec![Slot::Empty; cap],
+            free_list: Vec::new(),
         }
+    }
+
+    pub fn capacity(&self) -> PyResult<usize> {
+        // Return the current maximum capacity of the internal Rust Vectors.
+        Ok(self.capacity)
+    }
+
+    pub fn size(&self) -> PyResult<usize> {
+        // Return the current size of the internal Rust Vectors.
+        Ok(self.list_size)
+    }
+
+    pub fn percentage(&self) -> PyResult<f64> {
+        // Returns a floating-point number indicating the percentage of internal space occupied. 
+        let percent = (self.list_size as f64 / self.capacity as f64) * 100.0;
+        Ok(percent)
+    }
+
+    pub fn clear(&mut self) -> PyResult<()> {
+        // Clear all internal class variables -> Resetting the entire data structure.
+        self.list_size = 0;
+        self.head = 0;
+        self.tail = 0;
+        self.list_array = vec![Slot::Empty; self.capacity];
+        Ok(())
     }
 }
 
