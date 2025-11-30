060e99bcb25a4407104b20b944e81c316c5819a1
Simon Stensgaard <STcena1@hotmail.com>
2025-11-30 15:35:47 +0100

    Foxed only wall at mathematics

--- src/snaplit/linear/py_tinyset.py
+++ src/snaplit/linear/py_tinyset.py
@@ -1,8 +1,27 @@
 #---------- Imports ----------
 
-from snaplit._rust_snaplit import Stack as 
-from snaplit.rust_snaplit import
+from _rust_snaplit import TinySet as _RustTinySet
 
 from typing import Any, Optional, List, Iterator
 
-#---------- Linked List Shim ----------
+#---------- Tiny Set Shim ----------
+
+class TinySet():
+    def __init__(self, capacity: int = 128, threshold: float = 80.0):
+        self._inner = _RustTinySet(capacity, threshold)
+
+    def add(self, value: Any) -> bool:
+        return self._inner.add(value)
+    
+    def pop(self) -> Optional[Any]:
+        return self._inner.pop()
+    
+    def remove(self, index: int) -> Optional[Any]:
+        return self._inner.remove(index)
+    
+    def contains(self, value: Any) -> Any:
+        return self._inner.contains(value)
+    
+    def get(self, index: int) -> Any:
+        return self._inner.index()
+    
